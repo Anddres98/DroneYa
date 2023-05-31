c3c9c77b2b7cd1aa8c3e448952d216590ed939bb
Andres pardo <fernando.andresp13@gmail.com>
2023-05-31 16:43:11 -0500

    se agrego service de enviar correo con y sin adjunto

--- src/main/java/com/personal/droneya/DroneyaApplication.java
+++ src/main/java/com/personal/droneya/DroneyaApplication.java
@@ -1,13 +1,31 @@
 package com.personal.droneya;
 
+import com.personal.droneya.service.impl.EmeilSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DroneyaApplication {
 
+	@Autowired
+	private EmeilSenderService service;
 	public static void main(String[] args) {
 		SpringApplication.run(DroneyaApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail(){
+		service.sendSimpleEmail("Aquien se va enviar",
+				"contenido del mail",
+				"el asunto del mail");
+
+		service	.sendEmailWhitAttachment("emeil donde va llegar",
+				"this is Email body whit attachment",
+				"this email has attachment",
+				"Aqui ira el adjunto que desea enviar");
 	}
 
 }
--- src/main/java/com/personal/droneya/service/impl/EmeilSenderService.java
+++ src/main/java/com/personal/droneya/service/impl/EmeilSenderService.java
@@ -1,7 +1,63 @@
 package com.personal.droneya.service.impl;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmeilSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String toEmail,
+                                String body,
+                                String subject){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("Emeil de donde se envia");
+        message.setTo(toEmail);
+        message.setTo(body);
+        message.setTo(subject);
+
+        mailSender.send(message);
+        System.out.println("Mail send...");
+    }
+
+    public void sendEmailWhitAttachment(String toEmail,
+                                        String body,
+                                        String subject,
+                                        String attachment) throws MessagingException {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper =
+                new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom("De que correo sale");
+        //El toEmil es para cual correo se va enviar
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+        FileSystemResource fileSystem =
+                new FileSystemResource(new File(attachment));
+
+        mimeMessageHelper.addAttachment(fileSystem.getFilename(),
+                fileSystem);
+
+        mailSender.send(mimeMessage);
+        System.out.println("mail send whit attachment");
+
+    }
+
 }
+
